961ede88cc1ff6ec53e903397b596b0af0bcbee1
fabianwilliams <fabian@adotob.com>
2013-10-18 11:55:05 -0400

    External Content Type NorthWind .NET Assy
    dot net assembly with VS2012

---
+++ NorthWindEmployees/NorthWindEmployees/Features/NWindFarmECTEmpCRUD/NWindFarmECTEmpCRUD.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="0b28aa9d-586b-4635-8b2b-e615e05f3c72" featureId="0b28aa9d-586b-4635-8b2b-e615e05f3c72" imageUrl="" scope="Farm" solutionId="00000000-0000-0000-0000-000000000000" title="NorthWindEmployees Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="566198e0-6f83-4b87-85e2-24ac4a82ab88" />
+  </projectItems>
+</feature>
---
+++ NorthWindEmployees/NorthWindEmployees/NWindFarmECTEmployees/EmployeeService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NorthWindEmployees;
+
+namespace NorthWindEmployees.NWindFarmECTEmployees
+{
+    public partial class EmployeeService
+    {
+        public static Employee ReadItem(int employeeID)
+        {
+            NorthWindDataContext dataContext = new NorthWindDataContext
+            ("Data Source=Farm1Server1ADSQL;Initial Catalog=Northwind;uid=BCSUser1;pwd=YourPasswordHere");
+
+            Employee Employee =
+            (from employees in dataContext.Employees.AsEnumerable().Take(20)
+             where employees.EmployeeID == employeeID
+             select employees).Single();
+            return Employee;
+        }
+
+        public static IEnumerable<Employee> ReadList()
+        {
+            NorthWindDataContext dataContext = new NorthWindDataContext
+            ("Data Source=Farm1Server1ADSQL;Initial Catalog=Northwind;uid=BCSUser1;pwd=YourPasswordHere");
+
+
+            IEnumerable<Employee> Employees =
+                from employees in dataContext.Employees
+                select employees;
+            return Employees;
+        }
+
+        public static Employee Create(Employee newEmployee)
+        {
+            NorthWindDataContext dataContext = new NorthWindDataContext
+            ("Data Source=Farm1Server1ADSQL;Initial Catalog=Northwind;uid=BCSUser1;pwd=YourPasswordHere");
+
+
+            Employee emp = new Employee();
+
+            emp.FirstName = newEmployee.FirstName;
+            emp.LastName = newEmployee.LastName;
+            emp.Title = newEmployee.Title;
+            emp.TitleOfCourtesy = newEmployee.TitleOfCourtesy;
+            emp.BirthDate = newEmployee.BirthDate;
+            emp.HireDate = newEmployee.HireDate;
+            emp.Address = newEmployee.Address;
+            emp.City = newEmployee.City;
+            emp.Region = newEmployee.Region;
+            emp.PostalCode = newEmployee.PostalCode;
+            emp.Country = newEmployee.Country;
+            emp.HomePhone = newEmployee.HomePhone;
+            emp.Extension = newEmployee.Extension;
+            emp.Notes = newEmployee.Notes;
+
+            dataContext.Employees.InsertOnSubmit(emp);
+            dataContext.SubmitChanges();
+            return emp;
+        }
+
+        public static void Update(Employee employee, int parameter)
+        {
+            NorthWindDataContext dataContext = new NorthWindDataContext
+            ("Data Source=Farm1Server1ADSQL;Initial Catalog=Northwind;uid=BCSUser1;pwd=YourPasswordHere");
+
+            var employeeToUpdate = (from employees in dataContext.Employees
+                                   where employees.EmployeeID == parameter
+                                   select employees).Single();
+
+            employeeToUpdate.FirstName = employee.FirstName;
+            employeeToUpdate.LastName = employee.LastName;
+            employeeToUpdate.Title = employee.Title;
+            employeeToUpdate.TitleOfCourtesy = employee.TitleOfCourtesy;
+            employeeToUpdate.BirthDate = employee.BirthDate;
+            employeeToUpdate.HireDate = employee.HireDate;
+            employeeToUpdate.Address = employee.Address;
+            employeeToUpdate.City = employee.City;
+            employeeToUpdate.Region = employee.Region;
+            employeeToUpdate.PostalCode = employee.PostalCode;
+            employeeToUpdate.Country = employee.Country;
+            employeeToUpdate.HomePhone = employee.HomePhone;
+            employeeToUpdate.Extension = employee.Extension;
+            employeeToUpdate.Notes = employee.Notes;
+            dataContext.SubmitChanges();
+        }
+
+        public static void Delete(int employeeID)
+        {
+            NorthWindDataContext dataContext = new NorthWindDataContext
+            ("Data Source=Farm1Server1ADSQL;Initial Catalog=Northwind;uid=BCSUser1;pwd=YourPasswordHere");
+
+            Employee Employee =
+            (from employees in dataContext.Employees.AsEnumerable().Take(20)
+             where employees.EmployeeID == employeeID
+             select employees).Single();
+
+
+            dataContext.Employees.DeleteOnSubmit(Employee);
+            dataContext.SubmitChanges();
+
+        }
+    }
+}
